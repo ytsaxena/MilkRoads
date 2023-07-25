8f2ab7f8633b6a8bddc85dbdcaf5bda898a22780
Sachin Saxena <sssaxena058@gmail.com>
2023-07-25 07:59:59 +0530

    milkroads

---
+++ app/src/main/java/me/amitshekhar/mvvm/ui/topheadline/TopHeadlineViewModel.kt
@@ -0,0 +1,36 @@
+package me.amitshekhar.mvvm.ui.topheadline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import me.amitshekhar.mvvm.data.model.Article
+import me.amitshekhar.mvvm.data.repository.TopHeadlineRepository
+import me.amitshekhar.mvvm.ui.base.UiState
+import me.amitshekhar.mvvm.utils.AppConstant.COUNTRY
+
+class TopHeadlineViewModel(private val topHeadlineRepository: TopHeadlineRepository) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<List<Article>>>(UiState.Loading)
+
+    val uiState: StateFlow<UiState<List<Article>>> = _uiState
+
+    init {
+        fetchTopHeadlines()
+    }
+
+    private fun fetchTopHeadlines() {
+        viewModelScope.launch {
+            topHeadlineRepository.getTopHeadlines(COUNTRY)
+                .catch { e ->
+                    _uiState.value = UiState.Error(e.toString())
+                }
+                .collect {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+    }
+
+}
