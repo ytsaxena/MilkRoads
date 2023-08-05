cb6bc4453a63dd4c876320f1f2fd328cfebb4e31
Sachin Saxena <sssaxena058@gmail.com>
2023-08-05 20:00:08 +0530

    otp

---
+++ app/src/main/java/me/amitshekhar/mvvm/ui/OTPActivity.kt
@@ -0,0 +1,12 @@
+package me.amitshekhar.mvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.amitshekhar.mvvm.R
+
+class OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpactivity)
+    }
+}
